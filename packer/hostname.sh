77ebca9396010ec33957e4bfa7c12a061358ef4e
davejbax <davejbax@gmail.com>
2024-04-21 00:16:01 +0100

    Add script to change image hostname
    This is useful so that it's easy to modify the golden image very
    slightly in order to remove *any* manual step needed for installing it
    on a Pi. That is, the workflow becomes:
    
    1. Build Packer image
    2. Run ./hostname.sh
    3. Burn image to USB
    4. Plug USB into Pi and wait for it to boot & K3s to come up
    5. ???
    6. Profit

---
+++ packer/hostname.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+readonly mountpoint="/mnt/raspi-hostname"
+readonly image=${1-}
+readonly hostname=${2-}
+
+if [[ -z "${image}" ]] || [[ -z "${hostname}" ]]; then
+    echo >&2 "Usage: $0 <image> <hostname>"
+    exit 1
+fi
+
+if [[ "${EUID}" -ne 0 ]]; then
+    echo >&2 "This script can only be run as root"
+    exit 2
+fi
+
+mkdir -p "${mountpoint}"
+losetup -o 269484032 /dev/loop0 "${image}"
+mount /dev/loop0 "${mountpoint}"
+echo "${hostname}" > "${mountpoint}/etc/hostname"
+echo "127.0.0.1 ${hostname}" >> "${mountpoint}/etc/hosts"
+echo "::1 ${hostname}" >> "${mountpoint}/etc/hosts"
+umount "${mountpoint}"
+losetup -d /dev/loop0
+rm -d "${mountpoint}"
